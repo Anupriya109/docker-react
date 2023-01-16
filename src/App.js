3cc7d6dda058ef94bfe621f696ff2878198e9a0b
Anupriya Bishnoi <bishnoi@A-mac.local>
2023-01-16 15:03:40 +0530

    Change the app welcome message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Anu ! This is changed :)
+          Hello Anu ! This is changed for feature branch:)
         </p>
         <a
           className="App-link"
